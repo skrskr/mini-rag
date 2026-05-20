f3b86f3e6b7275c5b1a49a1c86dfef10a898ba8c
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2026-05-21 01:06:26 +0300

    move routes to nested router

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
-
+from routes import base
 
 app = FastAPI()
-
-@app.get('/welcome')
-def welcome():
-    return {
-        "message": "Hello world!"
-    }
+app.include_router(base.base_router)
