cdf4dde5333d012e4eaf4d67dcf103ea00f36ea7
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2026-06-09 19:57:04 +0300

    Add LLM factory to support multiple llm providers apis

--- src/main.py
+++ src/main.py
@@ -1,20 +1,32 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
-@app.on_event("shutdown")
-async def shutdown_db_client():
+    llm_provider_factory = LLMProviderFactory(settings)
+
+    # generation client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+
+    # embedding client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+
+    yield
+
     app.mongo_conn.close()
 
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
